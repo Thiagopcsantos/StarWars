95e9e56eac602da8d90da616298fa8b1943829c9
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-04-16 01:33:36 +0000

    primer comit

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -4,7 +4,61 @@ import { Link } from "react-router-dom";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<a className="navbar-brand">Navbar</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNavDropdown"
+					aria-controls="navbarNavDropdown"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon" />
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarNavDropdown">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a className="nav-link">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link">Features</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link">Pricing</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								id="navbarDropdownMenuLink"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Dropdown link
+							</a>
+							<div
+								className="dropdown-menu"
+								aria-labelledby="navbarDropdownMenuLink">
+								<a className="dropdown-item">Action</a>
+								<a className="dropdown-item">Another action</a>
+								<a className="dropdown-item">
+									Something else here
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+/*<nav className="navbar navbar-light bg-light mb-3">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">
 						React Webapp Boilerplate
@@ -17,7 +71,4 @@ export class Navbar extends React.Component {
 						</button>
 					</Link>
 				</div>
-			</nav>
-		);
-	}
-}
+			</nav>*/
--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -10,26 +10,27 @@ import injectContent from "./store/appContext.jsx";
 import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer.jsx";
 
+import { Link } from "react-router-dom";
+
 //create your first component
 export class Layout extends React.Component {
 	render() {
-		return (
-			<div className="d-flex flex-column h-100">
-				<BrowserRouter>
-					<ScrollToTop>
-						<Navbar />
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/demo" component={Demo} />
-							<Route path="/single/:theid" component={Single} />
-							<Route render={() => <h1>Not found!</h1>} />
-						</Switch>
-						<Footer />
-					</ScrollToTop>
-				</BrowserRouter>
-			</div>
-		);
+		return <Navbar />;
 	}
 }
 
 export default injectContent(Layout);
+/*	<div className="d-flex flex-column h-100">
+					<BrowserRouter>
+						<ScrollToTop>
+							<Navbar />
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route path="/demo" component={Demo} />
+								<Route path="/single/:theid" component={Single} />
+								<Route render={() => <h1>Not found!</h1>} />
+							</Switch>
+							<Footer />
+						</ScrollToTop>
+					</BrowserRouter>
+				</div>*/
